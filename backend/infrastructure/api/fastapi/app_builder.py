7dca948de63a4da4a3351926722a1bac6d3c0c43
Alejandro Garcia <alejandro.garcia@intelygenz.com>
2026-01-25 11:57:09 +0100

    modify cors

--- backend/infrastructure/api/fastapi/app_builder.py
+++ backend/infrastructure/api/fastapi/app_builder.py
@@ -97,8 +97,9 @@ class AppBuilder:
         # Add CORS middleware (adjust for production)
         app.add_middleware(
             CORSMiddleware,
-            allow_origins=["http://localhost:3000"],
-            allow_credentials=True,
+            allow_origins=["*"],
+            # allow_origins=["http://localhost:3000"],
+            # allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
         )
